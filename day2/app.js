ad95ee49a40fe0d9ad296260c101a8c8b117e732
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-07-25 23:09:14 +0545

    Create POST request

--- day2/app.js
+++ day2/app.js
@@ -1,7 +1,8 @@
 //Dependencies
-const express = require(express);
-const mongoose = require(mongoose);
-const Records = require("./models/records")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Records = require("./models/records");
 
 mongoose.connect("mongodb://localhost/CodingChallenge2");
 
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/CodingChallenge2");
 const app = express();
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //App routes
 app.get("/", (req, res) => {
@@ -17,6 +19,28 @@ app.get("/", (req, res) => {
 
 app.get("/create", (req, res) => {
     res.render("create");
+});
+
+app.post("/create", (req, res) => {
+    var newRecord = {
+        _id: 1,
+        name: req.body.name,
+        address: req.body.address,
+        gender: req.body.gender,
+        salary: req.body.salary,
+        dob: req.body.dob,
+        startdate: req.body.startdate,
+        post: req.body.post,
+        contactno: req.body.contactno,
+        qualifications: req.body.qualifications
+    }
+    Records.create(newRecord, (err, newlyCreated) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
 app.listen(8080, () => {
